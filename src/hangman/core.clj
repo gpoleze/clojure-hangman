74acb6d649748d06c173507f6e8a472a12cb86f7
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2019-08-10 13:16:01 -0700

    Improved the game strategy and added an end to it

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -1,25 +1,50 @@
 (ns hangman.core)
 
 (def max-number-lifes 6)
+(declare game)
 
-(defn lost [] (print "You were hang!"))
-(defn won [] (print "Congrats, you discovered the word!"))
+(defn game-over [message]
+  (do
+    (println message)
+    (System/exit 0)
+    )
+  )
+
+(defn lost [] ((game-over "You were hang!")))
+(defn won [] (game-over "Congrats, you discovered the word!"))
 
 (defn missing-letters [word guesses]
   (remove (fn [letter] (contains? guesses (str letter))) word)
   )
 
-(defn guessed-word? [word guesses]
+(defn was-word-guessed? [word guesses]
   (empty? (missing-letters word guesses))
   )
 
+(defn read-letter! [] (read-line))
+
+(defn is-guess-right? [word guess] (.contains word guess))
+
+(defn evaluate-guess [guess lifes word guesses]
+  (if (is-guess-right? word guess)
+    (game lifes word (conj guesses guess))
+    (game (dec lifes) word guesses)
+    )
+  )
+
 (defn game [lifes word guesses] (
-                    (if (= lifes 0)
-                      (lost)
-                      (if (guessed-word? word guesses)
-                        (won)
-                        (print "Try another letter")
-                        )
-                      )
-                    )
+                                 (if (= lifes 0)
+                                   (lost)
+                                   (if (was-word-guessed? word guesses)
+                                     (won)
+                                     (evaluate-guess (read-letter!) lifes word guesses)
+                                     )
+                                   )
+                                 )
+  )
+
+(defn -main
+  "Start the game"
+  [& args]
+  (game max-number-lifes "lime" #{})
   )
--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -15,11 +15,18 @@
 
 (deftest guessed-word?-test
   (testing "All letters are present"
-    (is (true? (guessed-word? "LEMON" #{"L" "E" "M" "O" "N"}))))
+    (is (true? (was-word-guessed? "LEMON" #{"L" "E" "M" "O" "N"}))))
   (testing "One Letter is missing"
-    (is (false? (guessed-word? "LEMON" #{"L" "E" "M" "O"}))))
+    (is (false? (was-word-guessed? "LEMON" #{"L" "E" "M" "O"}))))
   (testing "All Letters are missing"
-    (is (false? (guessed-word? "LEMON" #{}))))
+    (is (false? (was-word-guessed? "LEMON" #{}))))
   (testing "All Letters are missing but with wrong guesses"
-    (is (false? (guessed-word? "LEMON" #{"A" "X" "Y" "W"}))))
+    (is (false? (was-word-guessed? "LEMON" #{"A" "X" "Y" "W"}))))
+  )
+
+(deftest is-guess-right?-test
+  (testing "Letter is present in the word"
+    (is (true? (is-guess-right? "LEMON" "N"))))
+  (testing "Letter is not present in the word"
+    (is (false? (is-guess-right? "LEMON" "X"))))
   )
